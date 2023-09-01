a733978348181532d919e3b0bdcac37c90cc38ac
karam27 <karamrafeek2001@gmail.com>
2023-09-01 18:49:01 +0300

    CategoryTableSeeder

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            'name' => 'Category 2',
+            'slug' => Str::slug('Category 2'),
+            'description' => 'Category description text',
+            'parent_id' => 1,
+        ]);
+    }
+}
